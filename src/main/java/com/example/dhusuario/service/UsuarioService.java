0882cf7872f92beec18b6f5958296bcec34ed32a
Tulio Viglione <tulio.l.viglione@gmail.com>
2022-06-03 18:02:57 -0300

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .mvn/wrapper/maven-wrapper.jar
    	new file:   .mvn/wrapper/maven-wrapper.properties
    	new file:   mvnw
    	new file:   mvnw.cmd
    	new file:   pom.xml
    	new file:   src/main/java/com/example/dhusuario/DhUsuarioApplication.java
    	new file:   src/main/java/com/example/dhusuario/controller/UsuarioController.java
    	new file:   src/main/java/com/example/dhusuario/service/UsuarioService.java
    	new file:   src/main/java/com/example/dhusuario/service/UsuarioServiceImpl.java
    	new file:   src/main/resources/application.yml
    	new file:   src/test/java/com/example/dhusuario/DhUsuarioApplicationTests.java

---
+++ src/main/java/com/example/dhusuario/service/UsuarioService.java
@@ -0,0 +1,5 @@
+package com.example.dhusuario.service;
+
+public interface UsuarioService {
+    String getUsuario();
+}
---
+++ src/test/java/com/example/dhusuario/DhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
